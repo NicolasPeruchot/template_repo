6d05d8bfe605ca5fb2e659f22ae7a899c1daf553
“NicolasPeruchot” <nicolas.peruchot@gmail.com>
2022-04-22 09:16:08 +0200

    Update versions

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,6 +1,6 @@
 """Manage dependencies."""
 import pathlib
-
+from typing import List
 from setuptools import find_packages, setup
 
 
@@ -10,34 +10,35 @@ def _read(fname: str) -> str:
     return data
 
 
-base_packages = [
-
-]
+base_packages: List[str] = []
 
 dev_packages = [
-    "black==21.10b0",
-    "darglint==1.4.0",
-    "flake8>=3.9.2",
-    "flake8-bandit>=2.1.2",
-    "flake8-annotations>=2.7.0",
-    "flake8-bugbear>=21.9.2",
-    "flake8-docstrings>=1.6.0",
-    "ipykernel==6.5.0",
-    "isort>=5.10.0",
-    "pre-commit==2.15.0",
+    "black",
+    "darglint",
+    "flake8",
+    "flake8-bandit",
+    "flake8-annotations",
+    "flake8-bugbear",
+    "flake8-docstrings",
+    "ipykernel",
+    "isort",
+    "pre-commit",
 ]
 
 
 test_packages = [
-    "pytest>=6.2.5",
+    "pytest",
 ]
 
 
 setup(
-    name="{{ cookiecutter.project }}",
+    name="Test",
     version="0.0.1",
-    packages=find_packages(exclude=["notebooks","tests"]),
+    packages=find_packages(exclude=["notebooks", "tests"]),
     long_description=_read("README.md"),
     install_requires=base_packages,
-    extras_require={"dev": dev_packages + test_packages, "test": test_packages,},
+    extras_require={
+        "dev": dev_packages + test_packages,
+        "test": test_packages,
+    },
 )
